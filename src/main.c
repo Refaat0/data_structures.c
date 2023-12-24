cf4d2195e3f176ce5d74daba357d7b0c20dc2609
Refaat <elhajj.refaat@gmail.com>
2023-12-24 12:05:51 -0700

    Cleanup

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
+#include "../tests/test-doubly-linked-list-node.h"
 #include "../tests/test-doubly-linked-list.h"
 #include "../tests/test-array-list.h"
 #include "../tests/test-stack.h"
 #include "../tests/test-queue.h"
 
-#include <stdlib.h>
-
 int main(void)
 {
+    test_list();
+    test_linked_list();
     test_stack();
+    test_queue();
     return 0;
 }
