a6f321fb7cdad41045968561e3dc958aaa39aaf6
Refaat <elhajj.refaat@gmail.com>
2023-12-27 03:35:11 -0700

    Cleaning

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
 #include "../tests/test-doubly-linked-list-node.h"
 #include "../tests/test-doubly-linked-list.h"
+#include "../tests/test-comparator.h"
 #include "../tests/test-array-list.h"
 #include "../tests/test-stack.h"
 #include "../tests/test-queue.h"
-#include "../tests/test-comparator.h"
+
+#include "../include/doubly-linked-list.h"
 #include "../include/comparator.h"
+#include "../include/array-list.h"
+#include "../include/stack.h"
+#include "../include/queue.h"
 
 int main(void)
 {
